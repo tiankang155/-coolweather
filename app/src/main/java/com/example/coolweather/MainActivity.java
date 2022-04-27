fb159f0aab430aafa1d288476b0ad5cad45ce3a7
txk1034@175game.com <txk1034@175game.com>
2022-04-27 17:33:22 +0800

    First Commit

---
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.coolweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// /Users/tiankang/.ssh/id_rsa.pub
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
