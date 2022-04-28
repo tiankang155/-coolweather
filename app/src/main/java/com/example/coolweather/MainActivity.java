a069eae72a0effa72da8ebdc979991d1ecd01a37
txk1034@175game.com <txk1034@175game.com>
2022-04-28 15:27:17 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-// /Users/tiankang/.ssh/id_rsa.pub
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
-//    git remote set-url origin https://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@github.com/XiaoFengSophia/token.git
